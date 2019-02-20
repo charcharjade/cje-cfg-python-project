1507bbcbc730d97b297f080fd7ee1ffe97c8ad8b
Charlotte <cjeynon@hotmail.co.uk>
2019-02-20 19:46:59 +0000

    Edit first line

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # original testing
-print("Hello world! ... Edited")
+print("Hello world! ... ")
 print("Hi, my name is Kaylee!")
 #print("This is a test to see how good I am at this")
 #print("This is a test".title())
