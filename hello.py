ed3b20c870c464c6585a2971b01b16718c0d02d6
Charlotte <cjeynon@hotmail.co.uk>
2019-02-20 19:27:25 +0000

    Add hello world
    First repository commit.

---
+++ hello.py
@@ -0,0 +1,24 @@
+# original testing
+print("Hello world!")
+#print("This is a test to see how good I am at this")
+#print("This is a test".title())
+#age=22
+#hobby="swimming"
+#description="her age is {} and she likes {}.".format(age, hobby)
+#print (description)
+
+#testing counting method
+#the="the"
+#test="the quick brown fox jumped over the lazy dog".count(the)
+#print (test)
+
+#testing upper method
+#example="the two boys".upper()
+#print (example)
+
+#testing a sentence
+#name= "Charlotte"
+#age= 22
+#favourite_food="pizza"
+#description="My name is {}, I am {} years old and my favourite food is {}".format(name, age, favourite_food)
+#print (description)
